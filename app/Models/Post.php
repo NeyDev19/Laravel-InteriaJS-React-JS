b1b401360e74ad8b5ba8c809147084705771a401
neydev19 <moh.designer10@gmail.com>
2024-12-24 21:04:08 +0100

    up

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['body'];
 }
---
+++ resources/js/Pages/Create.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useForm } from '@inertiajs/react'
+
+
+const create = () => {
+    const { data, setData, post, processing, errors } = useForm({
+        body: '',
+    });
+    function submit(e) {
+        e.preventDefault()
+        post('/posts')
+      }
+      
+      
+    return (
+      <div>
+      <div className='title'>Create a Post</div>
+      <div>
+              <form onSubmit={submit} className='w-1/2 mx-auto'>
+                    <textarea rows="10" value={data.body} onChange={e => setData('body', e.target.value)}></textarea>
+                    <button className='primary-btn mt-4'>Create Post</button>
+              </form>
+            </div>
+            </div>
+  )
+}
+
+export default create
