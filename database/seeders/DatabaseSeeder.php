36b696fd8419ebabd3a0c51352184a1c9ead52b5
neydev19 <moh.designer10@gmail.com>
2024-12-24 19:36:32 +0100

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        Post::factory(30)->create();
+        Post::factory(2)->create();
     }
 }
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,9 +4,9 @@ const Home = ({ posts }) => {
   console.log(posts);
 
   return (
-      <>
-      <div>
-        {posts.map(post => (
+      <div className='flex flex-col items-center'>
+      <div className='pt-8'>
+        {posts.data.map(post => (
 
           <div key={post.id} className='p-4 border-b'>
             <div className='text-sm text-slate-600'>
@@ -18,7 +18,16 @@ const Home = ({ posts }) => {
           
         ))}
       </div>
-      </>
+      <div className='py-8 px-4'>
+        {posts.links.map(link => (
+          link.url ?
+          <Link className='px-4 py-2 rounded text-white bg-black m-1' key={link.label} href={link.url} dangerouslySetInnerHTML={{__html: link.label}} />
+            :
+                        <span className='px-4 py-2 rounded text-white bg-gray-600 m-1' key={link.label} dangerouslySetInnerHTML={{__html: link.label}}></span>
+
+          ))}
+      </div>
+      </div>
             
   )
 }
